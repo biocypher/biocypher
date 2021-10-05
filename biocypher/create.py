a7d7423ba3f27f7b48df41e66d12d87789f40ae5
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2021-10-05 21:38:46 +0200

    Update create.py

--- biocypher/create.py
+++ biocypher/create.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-TEST AUTODOC 
+TEST AUTODOC
 
 This module is used for the creating a generic property graph database for use
 in biomedical research applications. It takes as inputs ordered collections of
