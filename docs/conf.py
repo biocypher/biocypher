95b268a954dcfa40a02fc48d686e9b4445d3c72e
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2021-10-07 19:54:18 +0200

    sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ author = 'Sebastian Lobentanzer'
 # ones.
 extensions = [
 'sphinx.ext.autodoc',
-'sphinx.ext.napoleon'
+'sphinx.ext.napoleon',
+'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
