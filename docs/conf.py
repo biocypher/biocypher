b302adf8b3b1743aab5d8cfd37ef1a3f4bdb06a4
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2021-10-08 08:31:49 +0200

    autodock_mock_imports neo4j

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,10 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+# -- Autodoc configuration ---------------------------------------------------
+
+autodoc_mock_imports = ["neo4j"]
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
