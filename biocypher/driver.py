072beee6718985ffdff43a4e4540bf7e1ee0c33f
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2021-12-29 15:41:47 +0100

    sphinx indentation

--- biocypher/driver.py
+++ biocypher/driver.py
@@ -224,25 +224,26 @@ class BaseDriver(object):
                 for instance for passing a parameter dictionary
 
         Returns:
-            neo4j.Record.data: the Neo4j response to the query, consumed
-                by the shorthand .data() function on the Result object
-            neo4j.ResultSummary: information about the Result returned
-                by the .consume() function on the Result object
+            tuple of two:
+                - neo4j.Record.data: the Neo4j response to the query, consumed
+                  by the shorthand .data() function on the Result object
+                - neo4j.ResultSummary: information about the Result returned
+                  by the .consume() function on the Result object
 
         Todo:
-            generalise? had to create conditionals for profiling, as
-                the returns are not equally important. the .data()
-                shorthand may not be applicable in all cases. should we
-                return the `Result` object directly plus the summary
-                object from .consume()?
-            From Docs: "Any query results obtained within a
+            - generalise? had to create conditionals for profiling, as
+              the returns are not equally important. the .data()
+              shorthand may not be applicable in all cases. should we
+              return the `Result` object directly plus the summary
+              object from .consume()?
+              - From Docs: "Any query results obtained within a
                 transaction function should be consumed within that
                 function, as connection-bound resources cannot be
                 managed correctly when out of scope. To that end,
                 transaction functions can return values but these
                 should be derived values rather than raw results."
-            use session.run() or individual transactions?
-            From Docs: "Transaction functions are the recommended
+            - use session.run() or individual transactions?
+              - From Docs: "Transaction functions are the recommended
                 form for containing transactional units of work.
                 When a transaction fails, the driver retry logic is
                 invoked. For several failure cases, the transaction
@@ -250,9 +251,10 @@ class BaseDriver(object):
                 server. These cases include connection issues,
                 server role changes (e.g. leadership elections)
                 and transient errors."
-            use write and read distinctions in calling transactions
-                ("access mode")?
-            use neo4j `@unit_of_work`?
+            - use write and read distinctions in calling transactions
+              ("access mode")?
+            - use neo4j `@unit_of_work`?
+
         """
 
         db = db or self._db_config["db"] or neo4j.DEFAULT_DATABASE
