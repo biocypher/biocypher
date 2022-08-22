fe986616889eeb2545a4efc89fe38e6cec1b3464
deeenes <turei.denes@gmail.com>
2022-08-22 02:18:21 +0200

    clearer structure for `Driver.__init__` + minor adjustments

--- biocypher/_driver.py
+++ biocypher/_driver.py
@@ -69,15 +69,20 @@ class Driver(neo4j_utils.Driver):
             Password of the Neo4j user.
         fetch_size:
             Optional; the fetch size to use in database transactions.
-        config:
-            Path to a YAML config file which provides the URI, user name
-            and password.
         wipe:
             Wipe the database after connection, ensuring the data is
             loaded into an empty database.
         increment_version:
             Whether to increase version number automatically and create a
             new BioCypher version node in the graph.
+        schema_config:
+            Path to a custom database schema configuration file.
+        delimiter:
+            Delimiter for CSV export.
+        quote_char:
+            String quotation character for CSV export.
+        array_delimiter:
+            Array delimiter for CSV exported contents.
     """
 
     def __init__(
@@ -87,81 +92,87 @@ class Driver(neo4j_utils.Driver):
         db_uri: Optional[str] = None,
         db_user: Optional[str] = None,
         db_passwd: Optional[str] = None,
-        config: Optional[str] = "neo4j.yaml",
         fetch_size: int = 1000,
         wipe: bool = False,
         offline: bool = False,
         increment_version=True,
-        user_schema_config_path: Optional[str] = None,
+        schema_config: Optional[str] = None,
         delimiter: Optional[str] = None,
         array_delimiter: Optional[str] = None,
         quote_char: Optional[str] = None,
     ):
-        if not driver:
-            db_name = db_name or _config("neo4j_db")
-            db_uri = db_uri or _config("neo4j_uri")
-            db_user = db_user or _config("neo4j_user")
-            db_passwd = db_passwd or _config("neo4j_pw")
 
-            self.db_delim = delimiter or _config("neo4j_delimiter")
-            self.db_adelim = array_delimiter or _config(
-                "neo4j_array_delimiter"
+        db_name = db_name or _config("neo4j_db")
+        db_uri = db_uri or _config("neo4j_uri")
+        db_user = db_user or _config("neo4j_user")
+        db_passwd = db_passwd or _config("neo4j_pw")
+        self.db_delim = delimiter or _config("neo4j_delimiter")
+        self.db_adelim = array_delimiter or _config("neo4j_array_delimiter")
+        self.db_quote = quote_char or _config("neo4j_quote_char")
+        self.offline = offline
+
+        if offline:
+
+            logger.info("Offline mode: no connection to Neo4j.")
+
+            self.db_meta = VersionNode(
+                from_config=True,
+                config_file=schema_config,
+                offline=True,
+                bcy_driver=self,
             )
-            self.db_quote = quote_char or _config("neo4j_quote_char")
 
-            self.offline = offline
+            self._db_config = {
+                "uri": db_uri,
+                "user": db_user,
+                "passwd": db_passwd,
+                "db": db_name,
+                "fetch_size": fetch_size,
+            }
 
-            if offline:
-                logger.info("Offline mode: no connection to Neo4j.")
+            self._db_name = db_name
+
+        else:
+
+            neo4j_utils.Driver.__init__(**locals())
+
+            # if db representation node does not exist or explicitly
+            # asked for wipe, create new graph representation: default
+            # yaml, interactive?
+            if wipe:
+
+                # get database version node ('check' module) immutable
+                # variable of each instance (ie, each call from the
+                # adapter to BioCypher); checks for existence of graph
+                # representation and returns if found, else creates new
+                # one
                 self.db_meta = VersionNode(
                     from_config=True,
-                    config_file=user_schema_config_path,
-                    offline=True,
+                    config_file=schema_config,
                     bcy_driver=self,
                 )
-                self._db_config = {
-                    "uri": db_uri,
-                    "user": db_user,
-                    "passwd": db_passwd,
-                    "db": db_name,
-                    "fetch_size": fetch_size,
-                }
-                self._db_name = db_name
+
+                self.init_db()
 
             else:
-                neo4j_utils.Driver.__init__(**locals())
-                # if db representation node does not exist or explicitly
-                # asked for wipe, create new graph representation: default
-                # yaml, interactive?
-                if wipe:
-                    # get database version node ('check' module) immutable
-                    # variable of each instance (ie, each call from the
-                    # adapter to BioCypher); checks for existence of graph
-                    # representation and returns if found, else creates new
-                    # one
-                    self.db_meta = VersionNode(
-                        from_config=True,
-                        config_file=user_schema_config_path,
-                        bcy_driver=self,
-                    )
-                    self.init_db()
-                else:
-                    self.db_meta = VersionNode(self)
+
+                self.db_meta = VersionNode(bcy_driver = self)
 
                 if increment_version:
+
                     # set new current version node
                     self.update_meta_graph()
 
-            self.bl_adapter = None
-            self.batch_writer = None
+        self.bl_adapter = None
+        self.batch_writer = None
+        self._update_translator()
 
-            self._update_translator()
+        # TODO: implement passing a driver instance
+        # I am not sure, but seems like it should work fine
 
-        else:
-            logger.error("Providing driver instance is not yet implemented.")
-            # TODO: implement passing a driver instance
 
     def update_meta_graph(self):
+
         logger.info("Updating Neo4j meta graph.")
         # add version node
         self.add_biocypher_nodes(self.db_meta)
@@ -283,7 +294,7 @@ class Driver(neo4j_utils.Driver):
         bn = self.translator.translate_nodes(id_type_tuples)
         return self.add_biocypher_nodes(bn)
 
-    def add_edges(self, src_tar_type_tuples):
+    def add_edges(self, src_tar_type_tuples: Iterable[tuple]) -> tuple:
         """
         Generic edge adder method to add any kind of input to the
         graph via the :class:`biocypher.create.BioCypherEdge` class.
@@ -310,8 +321,11 @@ class Driver(neo4j_utils.Driver):
         return self.add_biocypher_edges(bn)
 
     def add_biocypher_nodes(
-        self, nodes, explain: bool = False, profile: bool = False
-    ):
+            self,
+            nodes: Iterable[BioCypherNode],
+            explain: bool = False,
+            profile: bool = False,
+        ) -> bool:
         """
         Accepts a node type handoff class
         (:class:`biocypher.create.BioCypherNode`) with id,
@@ -327,15 +341,15 @@ class Driver(neo4j_utils.Driver):
         match and on create, irrespective of the actual event.
 
         Args:
-            nodes (iterable of BioCypherNode): a list of
-                :class:`biocypher.create.BioCypherNode` objects
-            explain (bool): whether to call ``EXPLAIN`` in front of the
-                CYPHER query
-            profile (bool): whether to call ``PROFILE`` in front of the
-                CYPHER query
+            nodes:
+                An iterable of :class:`biocypher.create.BioCypherNode` objects.
+            explain:
+                Call ``EXPLAIN`` on the CYPHER query.
+            profile:
+                Do profiling on the CYPHER query.
 
         Returns:
-            bool: The return value. True for success, False otherwise.
+            True for success, False otherwise.
         """
 
         # receive generator objects
@@ -387,8 +401,11 @@ class Driver(neo4j_utils.Driver):
             return res
 
     def add_biocypher_edges(
-        self, edges, explain: bool = False, profile: bool = False
-    ):
+            self,
+            edges: Iterable[BioCypherEdge],
+            explain: bool = False,
+            profile: bool = False,
+        ) -> bool:
         """
         Accepts an edge type handoff class
         (:class:`biocypher.create.BioCypherEdge`) with source
@@ -410,14 +427,15 @@ class Driver(neo4j_utils.Driver):
         create, irrespective of the actual event.
 
         Args:
-            edges: a list of :class:`biocypher.create.BioCypherEdge` objects
-            explain (bool): whether to call ``EXPLAIN`` in front of the
-                CYPHER query
-            profile (bool): whether to call ``PROFILE`` in front of the
-                CYPHER query
+            edges:
+                An iterable of :class:`biocypher.create.BioCypherEdge` objects.
+            explain:
+                Call ``EXPLAIN`` on the CYPHER query.
+            profile:
+                Do profiling on the CYPHER query.
 
         Returns:
-            bool: The return value. True for success, False otherwise.
+            `True` for success, `False` otherwise.
         """
 
         rel_as_node = False
