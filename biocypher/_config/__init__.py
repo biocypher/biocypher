32549870966f331aeb3d626be975b51115e0adf5
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2022-12-21 18:10:42 +0100

    allow local config in root as well as `config/` .. relative to cwd

--- biocypher/_config/__init__.py
+++ biocypher/_config/__init__.py
@@ -66,7 +66,14 @@ def read_config() -> dict:
     defaults = module_data('module_config')
     user_confdir = appdirs.user_config_dir('biocypher', 'saezlab')
     user = _read_yaml(os.path.join(user_confdir, 'biocypher_config.yaml')) or {}
-    local = _read_yaml('biocypher_config.yaml') or {}
+    # check if there is a local config file `biocypher_config.yaml` in the
+    # current working directory
+    if os.path.exists('biocypher_config.yaml'):
+        local = _read_yaml('biocypher_config.yaml')
+    elif os.path.exists('config/biocypher_config.yaml'):
+        local = _read_yaml('config/biocypher_config.yaml')
+    else:
+        local = {}
 
     defaults.update(user)
     defaults.update(local)
