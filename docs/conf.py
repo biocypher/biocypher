cec2d2bd2318ca069b6b459b0a927f7a790a89c7
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-03-02 23:38:49 +0100

    change sphinx dependency to `design`

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ extensions = [
     'sphinx.ext.todo',  # not for output but to remove warnings
     'myst_parser',  # markdown support
     'sphinx_rtd_theme',
-    'sphinx_panels',
+    'sphinx_design',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
