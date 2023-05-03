9b5380be492c4f51dcc5141ec53f4b2cc39092ba
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-05-03 02:52:46 +0200

    make intermediate lists temporary (function scope)

--- biocypher/_pandas.py
+++ biocypher/_pandas.py
@@ -6,7 +6,6 @@ class Pandas:
         self.ontology = ontology
         self.translator = translator
 
-        self.lists = {}
         self.dfs = {}
 
     def _separate_entity_types(self, entities):
@@ -14,23 +13,26 @@ class Pandas:
         Given mixed iterable of BioCypher objects, separate them into lists by
         type.
         """
+        lists = {}
         for entity in entities:
             if not isinstance(entity, BioCypherNode) and not isinstance(entity, BioCypherEdge):
                 raise TypeError(f"Expected a BioCypherNode or BioCypherEdge, got {type(entity)}.")
             
             _type = entity.get_label()
-            if not _type in self.lists:
-                self.lists[_type] = []
-            self.lists[_type].append(entity)
+            if not _type in lists:
+                lists[_type] = []
+            lists[_type].append(entity)
+
+        return lists
 
     def add_tables(self, entities):
         """
         Add Pandas dataframes for each node and edge type in the input.
         """
 
-        self._separate_entity_types(entities)
+        lists = self._separate_entity_types(entities)
 
-        for _type, _entities in self.lists.items():
+        for _type, _entities in lists.items():
             self._add_entity_df(_type, _entities)
 
     def _add_entity_df(self, _type, _entities):
