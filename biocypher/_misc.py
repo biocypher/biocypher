2444954e4664ee84143e6efcee51cd9541433896
Nils Krehl <nils.krehl@posteo.de>
2024-02-01 12:48:53 +0100

    #307: add warning log if multiple inheritance is present; improve the unit tests; refactoring

--- biocypher/_misc.py
+++ biocypher/_misc.py
@@ -76,48 +76,18 @@ def ensure_iterable(value: Any) -> Iterable:
     return value if isinstance(value, LIST_LIKE) else (value,)
 
 
-def create_tree_visualisation(inheritance_tree: Union[dict, nx.Graph]) -> str:
+def create_tree_visualisation(inheritance_graph: Union[dict, nx.Graph]) -> Tree:
     """
     Creates a visualisation of the inheritance tree using treelib.
     """
-
-    if isinstance(inheritance_tree, nx.Graph):
-        inheritance_tree = nx.to_dict_of_lists(inheritance_tree)
-        # unlist values
-        inheritance_tree = {k: v[0] for k, v in inheritance_tree.items() if v}
-
-    # find root node
-    classes = set(inheritance_tree.keys())
-    parents = set(inheritance_tree.values())
-    root = list(parents - classes)
-
-    if len(root) > 1:
-        if "entity" in root:
-            root = "entity"  # default: good standard? TODO
-
-        else:
-            raise ValueError(
-                "Inheritance tree cannot have more than one root node. "
-                f"Found {len(root)}: {root}."
-            )
-
-    else:
-        root = root[0]
-
-    if not root:
-        # find key whose value is None
-        root = list(inheritance_tree.keys())[
-            list(inheritance_tree.values()).index(None)
-        ]
+    inheritance_tree = _get_inheritance_tree(inheritance_graph)
+    classes, root = _find_root_node(inheritance_tree)
 
     tree = Tree()
-
     tree.create_node(root, root)
-
     while classes:
         for child in classes:
             parent = inheritance_tree[child]
-
             if parent in tree.nodes.keys() or parent == root:
                 tree.create_node(child, child, parent=parent)
 
@@ -126,6 +96,60 @@ def create_tree_visualisation(inheritance_tree: Union[dict, nx.Graph]) -> str:
                 classes.remove(node)
 
     return tree
+
+
+def _get_inheritance_tree(inheritance_graph: Union[dict, nx.Graph]) -> dict:
+    """Transforms an inheritance_graph into an inheritance_tree.
+
+    Args:
+        inheritance_graph: A dict or nx.Graph representing the inheritance graph.
+
+    Returns:
+        A dict representing the inheritance tree.
+    """
+    if isinstance(inheritance_graph, nx.Graph):
+        inheritance_tree = nx.to_dict_of_lists(inheritance_graph)
+
+        multiple_parents_present = _multiple_inheritance_present(
+            inheritance_tree
+        )
+        if multiple_parents_present:
+            logger.warning(
+                "The ontology contains multiple inheritance (one child node has multiple parent nodes). This is not visualized in the following hierarchy tree (the child node is only added once). If you want to browse all relationships of the parsed ontology write a graphml file to disk and view this file."
+            )
+
+        # unlist values
+        inheritance_tree = {k: v[0] for k, v in inheritance_tree.items() if v}
+        return inheritance_tree
+    elif not _multiple_inheritance_present(inheritance_graph):
+        return inheritance_graph
+
+
+def _multiple_inheritance_present(inheritance_tree: dict) -> bool:
+    """Checks if multiple inheritance is present in the inheritance_tree."""
+    return any(len(value) > 1 for value in inheritance_tree.values())
+
+
+def _find_root_node(inheritance_tree: dict) -> tuple[set, str]:
+    classes = set(inheritance_tree.keys())
+    parents = set(inheritance_tree.values())
+    root = list(parents - classes)
+    if len(root) > 1:
+        if "entity" in root:
+            root = "entity"  # default: good standard? TODO
+        else:
+            raise ValueError(
+                "Inheritance tree cannot have more than one root node. "
+                f"Found {len(root)}: {root}."
+            )
+    else:
+        root = root[0]
+    if not root:
+        # find key whose value is None
+        root = list(inheritance_tree.keys())[
+            list(inheritance_tree.values()).index(None)
+        ]
+    return classes, root
 
 
 # string conversion, adapted from Biolink Model Toolkit
--- test/test_misc.py
+++ test/test_misc.py
@@ -1,4 +1,7 @@
+import logging
+
 import pytest
+import treelib
 import networkx as nx
 
 from biocypher._misc import create_tree_visualisation
@@ -35,9 +38,16 @@ def _get_disjoint_tree() -> dict:
 def test_tree_vis(_get_inheritance_tree):
     tree_vis = create_tree_visualisation(_get_inheritance_tree)
 
+    expected_tree_vis = create_tree_visualisation(
+        nx.DiGraph(_get_inheritance_tree)
+    )
+
     assert tree_vis.DEPTH == 1
     assert tree_vis.WIDTH == 2
     assert tree_vis.root == "A"
+    assert tree_vis.to_json(with_data=True) == expected_tree_vis.to_json(
+        with_data=True
+    )
 
 
 def test_tree_vis_from_networkx(_get_inheritance_tree):
@@ -45,14 +55,49 @@ def test_tree_vis_from_networkx(_get_inheritance_tree):
 
     tree_vis = create_tree_visualisation(graph)
 
+    expected_tree_vis = create_tree_visualisation(
+        nx.DiGraph(_get_inheritance_tree)
+    )
+
     assert tree_vis.DEPTH == 1
     assert tree_vis.WIDTH == 2
     assert tree_vis.root == "A"
+    assert tree_vis.to_json(with_data=True) == expected_tree_vis.to_json(
+        with_data=True
+    )
 
 
 def test_disjoint_tree(_get_disjoint_tree):
     with pytest.raises(ValueError):
         create_tree_visualisation(_get_disjoint_tree)
+
+
+def test_tree_vis_multiple_inheritance(caplog):
+    inheritance_tree_data = {
+        "root": [],
+        "level1A": ["root"],
+        "level1B": ["root"],
+        "level2A": ["level1A", "level1B"],
+    }
+    inheritance_tree = nx.DiGraph(inheritance_tree_data)
+    with caplog.at_level(logging.INFO):
+        tree_vis = create_tree_visualisation(inheritance_tree)
+
+    expected_tree = {
+        "root": [],
+        "level1A": ["root"],
+        "level1B": ["root"],
+        "level2A": ["level1A"],
+    }
+    expected_tree_vis = create_tree_visualisation(nx.DiGraph(expected_tree))
+
+    assert any(
+        "The ontology contains multiple inheritance" in record.message
+        for record in caplog.records
+    )
+    assert tree_vis.to_json(with_data=True) == expected_tree_vis.to_json(
+        with_data=True
+    )
 
 
 if __name__ == "__main__":
