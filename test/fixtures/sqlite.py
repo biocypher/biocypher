4ec1891805dc9ed93a658f156bacbfb9ae2e2d2b
Nils Krehl <nils.krehl@posteo.de>
2024-03-11 10:09:49 +0100

    #314: fix teardwon for windows

--- test/fixtures/sqlite.py
+++ test/fixtures/sqlite.py
@@ -29,6 +29,8 @@ def remove_file(file_path: str):
     except PermissionError:
         # fix Windows error that once opened files can only be removed after a restart
         # https://groups.google.com/g/python-sqlite/c/2KBI2cR-t70
-        print("Permission error")
         os.rename(file_path, f"renamed_{file_path}")
         os.remove(f"renamed_{file_path}")
+    except FileNotFoundError:
+        # at the moment on windows the file is not created
+        pass
