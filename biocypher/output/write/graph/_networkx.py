179696f218d829d89e229ad3be729a71d06af4ab
slobentanzer <sebastian.lobentanzer@gmail.com>
2025-01-14 14:10:04 +0100

    docstrings

--- biocypher/output/write/graph/_networkx.py
+++ biocypher/output/write/graph/_networkx.py
@@ -8,7 +8,11 @@ from biocypher.output.in_memory._networkx import NetworkxKG
 class _NetworkXWriter(_Writer):
     """
     Class for writing the in-memory networkx DiGraph to file.
-    You need to call `_construct_import_call` to write the networkx DiGraph to a pickle file.
+
+    Call `_construct_import_call` to write the networkx DiGraph to a pickle
+    file and return the Python call to load it.
+
+    TODO: this is a non-intuitive name, should be adjusted.
     """
 
     def __init__(self, *args, **kwargs):
@@ -18,7 +22,7 @@ class _NetworkXWriter(_Writer):
         )
 
     def _construct_import_call(self) -> str:
-        """Function to construct the Python code to load the networkx graph from a pickle file.
+        """Dump networkx graph to a pickle file and return Python call.
 
         Returns:
             str: Python code to load the networkx graph from a pickle file.
@@ -39,9 +43,29 @@ class _NetworkXWriter(_Writer):
         return "import_networkx.py"
 
     def _write_node_data(self, nodes) -> bool:
+        """Add nodes to the networkx graph.
+
+        TODO: this is not strictly writing, should be refactored.
+
+        Args:
+            nodes (list): List of nodes to add to the networkx graph.
+
+        Returns:
+            bool: True if the nodes were added successfully, False otherwise.
+        """
         passed = self.in_memory_networkx_kg.add_nodes(nodes)
         return passed
 
     def _write_edge_data(self, edges) -> bool:
+        """Add edges to the networkx graph.
+
+        TODO: this is not strictly writing, should be refactored.
+
+        Args:
+            edges (list): List of edges to add to the networkx graph.
+
+        Returns:
+            bool: True if the edges were added successfully, False otherwise.
+        """
         passed = self.in_memory_networkx_kg.add_edges(edges)
         return passed
