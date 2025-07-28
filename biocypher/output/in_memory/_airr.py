049d926c85028e493a76600c3ea75acd85b88d4f
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2025-07-28 14:17:59 +0200

    pre-commit

--- biocypher/output/in_memory/_airr.py
+++ biocypher/output/in_memory/_airr.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Optional
 
 from biocypher._create import BioCypherEdge, BioCypherNode
 from biocypher._deduplicate import Deduplicator
