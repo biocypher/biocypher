49c1ee17662802ba803b54043d9dafebbc0c4105
Johann Dreo <75248710+jdreo@users.noreply.github.com>
2026-01-12 18:44:39 +0100

    fix(core/write_import_call): display an error message when no element is added (#464)
    Updated CI workflow to use MacOS latest, since 13 is no longer supported.
    
    * fix(core/write_import_call): display an error message when no element is added
    
    And tries to continue with calling the initialized writer.
    
    * convert error to warning (as we continue execution)
    
    * format
    
    * update macos to `latest` (13 is retired)
    
    ---------
    
    Co-authored-by: Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>

--- biocypher/_core.py
+++ biocypher/_core.py
@@ -688,6 +688,12 @@ class BioCypher:
         if not self._offline:
             msg = "Cannot write import call in online mode."
             raise NotImplementedError(msg)
+        else:
+            if not self._writer:
+                logger.warning(
+                    "No edges or nodes were added, I'll try to continue, but you may want to double-check your data."
+                )
+                self._initialize_writer()
 
         return self._writer.write_import_call()
 
